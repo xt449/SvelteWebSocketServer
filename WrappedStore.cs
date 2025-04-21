7a90aa9d84e57e2b2a6a5d219b1ddfe5af1aed65
Jonathan Talcott <19jtalcott97@gmail.com>
2025-04-21 16:40:57 -0400

    Added basic WrappedStore class for possibly easier accessing of stores

---
+++ WrappedStore.cs
@@ -0,0 +1,15 @@
+﻿namespace SvelteWebSocketServer;
+
+public class WrappedStore<T>(WebSocketWrapper wsw, string scope, string id)
+{
+	private readonly WebSocketWrapper wsw = wsw;
+
+	public readonly string scope = scope;
+	public readonly string id = id;
+
+	public T? Value
+	{
+		get => wsw.GetCachedOutgoingValue<T>(scope, id);
+		set => _ = wsw.SendValueAsync(scope, id, value);
+	}
+}
