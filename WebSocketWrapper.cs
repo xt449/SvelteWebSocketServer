77cf74d6fd26d1b76abc525ebc29c15a953ea8b9
Jonathan Talcott <19jtalcott97@gmail.com>
2025-04-21 13:58:04 -0400

    New namespace format

--- WebSocketWrapper.cs
+++ WebSocketWrapper.cs
@@ -1,115 +1,114 @@
-﻿using EmbedIO.WebSockets;
+﻿namespace SvelteWebSocketServer;
+
+using EmbedIO.WebSockets;
 using System.Collections.Concurrent;
 using System.Net.WebSockets;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 
-namespace SvelteWebSocketServer
+public class WebSocketWrapper : WebSocketModule
 {
-	public class WebSocketWrapper : WebSocketModule
+	/// <summary>
+	/// Track the values sent by the server so that new clients connecting can be instantly updated.
+	/// <br/>Key: store ID.
+	/// <br/>Value: store value as raw JSON string.
+	/// </summary>
+	private readonly ConcurrentDictionary<(string scope, string id), string> rawJsonStringStoresDictionary = new();
+
+	public event JsonSetHandler? OnJsonSet;
+
+	public WebSocketWrapper() : base("/", true)
 	{
-		/// <summary>
-		/// Track the values sent by the server so that new clients connecting can be instantly updated.
-		/// <br/>Key: store ID.
-		/// <br/>Value: store value as raw JSON string.
-		/// </summary>
-		private readonly ConcurrentDictionary<(string scope, string id), string> rawJsonStringStoresDictionary = new();
+	}
 
-		public event JsonSetHandler? OnJsonSet;
+	// Handlers
 
-		public WebSocketWrapper() : base("/", true)
+	protected override async Task OnClientConnectedAsync(IWebSocketContext context)
+	{
+		// On client connect, send all current stored values
+		foreach (((string scope, string id), string value) in rawJsonStringStoresDictionary)
 		{
+			await SendAsync(context, BuildMessageRaw(scope, id, value));
 		}
+	}
 
-		// Handlers
-
-		protected override async Task OnClientConnectedAsync(IWebSocketContext context)
+	protected override async Task OnMessageReceivedAsync(IWebSocketContext context, byte[] buffer, IWebSocketReceiveResult result)
+	{
+		// Only handle text-type messages
+		if (result.MessageType == (int)WebSocketMessageType.Text)
 		{
-			// On client connect, send all current stored values
-			foreach (((string scope, string id), string value) in rawJsonStringStoresDictionary)
+			JsonElement rootElement;
+			try
 			{
-				await SendAsync(context, BuildMessageRaw(scope, id, value));
+				rootElement = JsonDocument.Parse(Encoding.UTF8.GetString(buffer, 0, result.Count)).RootElement;
 			}
-		}
-
-		protected override async Task OnMessageReceivedAsync(IWebSocketContext context, byte[] buffer, IWebSocketReceiveResult result)
-		{
-			// Only handle text-type messages
-			if (result.MessageType == (int)WebSocketMessageType.Text)
+			catch
 			{
-				JsonElement rootElement;
-				try
-				{
-					rootElement = JsonDocument.Parse(Encoding.UTF8.GetString(buffer, 0, result.Count)).RootElement;
-				}
-				catch
-				{
-					// Abort on invalid json
-					return;
-				}
-
-				if (!rootElement.TryGetProperty("scope", out JsonElement scopeElement))
-				{
-					// Abort on missing property
-					return;
-				}
-
-				string? scope = scopeElement.GetString();
-
-				if (scope == null)
-				{
-					// Abort on null value
-					return;
-				}
-
-				if (!rootElement.TryGetProperty("id", out JsonElement idElement))
-				{
-					// Abort on missing property
-					return;
-				}
-
-				string? id = idElement.GetString();
-
-				if (id == null)
-				{
-					// Abort on null value
-					return;
-				}
-
-				if (!rootElement.TryGetProperty("value", out JsonElement valueElement))
-				{
-					// Abort on missing property
-					return;
-				}
-
-				// Trigger event
-				OnJsonSet?.Invoke(scope, id, valueElement);
+				// Abort on invalid json
+				return;
 			}
+
+			if (!rootElement.TryGetProperty("scope", out JsonElement scopeElement))
+			{
+				// Abort on missing property
+				return;
+			}
+
+			string? scope = scopeElement.GetString();
+
+			if (scope == null)
+			{
+				// Abort on null value
+				return;
+			}
+
+			if (!rootElement.TryGetProperty("id", out JsonElement idElement))
+			{
+				// Abort on missing property
+				return;
+			}
+
+			string? id = idElement.GetString();
+
+			if (id == null)
+			{
+				// Abort on null value
+				return;
+			}
+
+			if (!rootElement.TryGetProperty("value", out JsonElement valueElement))
+			{
+				// Abort on missing property
+				return;
+			}
+
+			// Trigger event
+			OnJsonSet?.Invoke(scope, id, valueElement);
 		}
+	}
 
-		// Accessors
+	// Accessors
 
-		public async Task SendValueAsync<T>(string scope, string id, T value)
-		{
-			string rawJsonString = JsonSerializer.Serialize(value);
+	public async Task SendValueAsync<T>(string scope, string id, T value)
+	{
+		string rawJsonString = JsonSerializer.Serialize(value);
 
-			// Set value locally
-			rawJsonStringStoresDictionary[(scope, id)] = rawJsonString;
+		// Set value locally
+		rawJsonStringStoresDictionary[(scope, id)] = rawJsonString;
 
-			// Distribute message to clients
-			await BroadcastAsync(BuildMessageRaw(scope, id, rawJsonString));
-		}
+		// Distribute message to clients
+		await BroadcastAsync(BuildMessageRaw(scope, id, rawJsonString));
+	}
 
-		public async Task SendGlobalValueAsync<T>(string id, T value) => await SendValueAsync("global", id, value);
+	public async Task SendGlobalValueAsync<T>(string id, T value) => await SendValueAsync("global", id, value);
 
-		// Helpers
+	// Helpers
 
-		public delegate void JsonSetHandler(string scope, string id, JsonElement value);
+	public delegate void JsonSetHandler(string scope, string id, JsonElement value);
 
-		private static string BuildMessageRaw(string scope, string id, string rawjsonString)
-		{
-			return $"{{\"scope\":\"{scope}\",\"id\":\"{id}\",\"value\":{rawjsonString}}}";
-		}
+	private static string BuildMessageRaw(string scope, string id, string rawjsonString)
+	{
+		return $"{{\"scope\":\"{scope}\",\"id\":\"{id}\",\"value\":{rawjsonString}}}";
 	}
 }
--- WebSocketWrapperListener.cs
+++ WebSocketWrapperListener.cs
@@ -1,124 +1,123 @@
-﻿using System;
+﻿namespace SvelteWebSocketServer;
+
+using System;
 using System.Collections.Generic;
 using System.Text.Json;
 using System.Text.RegularExpressions;
 
-namespace SvelteWebSocketServer
+public class WebSocketWrapperListener
 {
-	public class WebSocketWrapperListener
+	public delegate void JsonSetHandler(string scope, string id, JsonElement value);
+	public delegate void TypedSetHandler<T>(string scope, string id, T? value);
+
+	public delegate void RegexJsonSetHandler(string scope, Match match, JsonElement value);
+	public delegate void RegexTypedSetHandler<T>(string scope, Match match, T? value);
+
+	private readonly Dictionary<string, List<JsonSetHandler>> jsonHandlers = new();
+	private readonly Dictionary<string, List<Delegate>> typedHandlers = new();
+
+	private readonly List<(Regex regex, RegexJsonSetHandler handler)> regexJsonHandlers = new();
+	private readonly List<(Regex regex, Delegate handler)> regexTypedHandlers = new();
+
+	public WebSocketWrapperListener(WebSocketWrapper wsw)
 	{
-		public delegate void JsonSetHandler(string scope, string id, JsonElement value);
-		public delegate void TypedSetHandler<T>(string scope, string id, T? value);
-
-		public delegate void RegexJsonSetHandler(string scope, Match match, JsonElement value);
-		public delegate void RegexTypedSetHandler<T>(string scope, Match match, T? value);
-
-		private readonly Dictionary<string, List<JsonSetHandler>> jsonHandlers = new();
-		private readonly Dictionary<string, List<Delegate>> typedHandlers = new();
-
-		private readonly List<(Regex regex, RegexJsonSetHandler handler)> regexJsonHandlers = new();
-		private readonly List<(Regex regex, Delegate handler)> regexTypedHandlers = new();
-
-		public WebSocketWrapperListener(WebSocketWrapper wsw)
-		{
-			wsw.OnJsonSet += OnJsonSet;
-		}
-
-		private void OnJsonSet(string scope, string id, JsonElement jsonElement)
-		{
-			// Invoke handlers that process raw JSON values for exact ID matches
-			if (this.jsonHandlers.TryGetValue(id, out List<JsonSetHandler>? jsonHandlers))
-			{
-				foreach (JsonSetHandler handler in jsonHandlers)
-				{
-					handler(scope, id, jsonElement);
-				}
-			}
-
-			// Invoke handlers that process deserialized values for exact ID matches
-			if (this.typedHandlers.TryGetValue(id, out List<Delegate>? typedHandlers))
-			{
-				foreach (Delegate handler in typedHandlers)
-				{
-					// Get type dynamically from handler
-					Type targetType = handler.GetType().GenericTypeArguments[0];
-					// Deserialize jsonElement with type
-					object? deserializedValue = JsonSerializer.Deserialize(jsonElement, targetType);
-					// Invoke handler
-					handler.DynamicInvoke(scope, id, deserializedValue);
-				}
-			}
-
-			// Invoke handlers that process raw JSON values for regex matched IDs
-			foreach ((Regex regex, RegexJsonSetHandler handler) in regexJsonHandlers)
-			{
-				Match match = regex.Match(id);
-
-				if (match.Success)
-				{
-					handler(scope, match, jsonElement);
-				}
-			}
-
-			// Invoke handlers that process deserialized values for regex matched IDs
-			foreach ((Regex regex, Delegate handler) in regexTypedHandlers)
-			{
-				Match match = regex.Match(id);
-
-				if (match.Success)
-				{
-					// Get type dynamically from handler
-					Type targetType = handler.GetType().GenericTypeArguments[0];
-					// Deserialize jsonElement with type
-					object? deserializedValue = JsonSerializer.Deserialize(jsonElement, targetType);
-					// Invoke handler
-					handler.DynamicInvoke(scope, match, deserializedValue);
-				}
-			}
-		}
-
-		// Publics
-
-		/// <summary>
-		/// Adds a handler for processing raw JSON values where there is an exact ID match.
-		/// </summary>
-		public void AddHandler(string id, JsonSetHandler handler)
-		{
-			if (jsonHandlers.TryGetValue(id, out List<JsonSetHandler>? handlers))
-			{
-				handlers.Add(handler);
-			}
-			else
-			{
-				jsonHandlers[id] = [handler];
-			}
-		}
-
-		/// <summary>
-		/// Adds a handler for processing deserialized values where there is an exact ID match.
-		/// </summary>
-		public void AddHandler<T>(string id, TypedSetHandler<T> handler)
-		{
-			if (typedHandlers.TryGetValue(id, out List<Delegate>? handlers))
-			{
-				handlers.Add(handler);
-			}
-			else
-			{
-				typedHandlers[id] = [handler];
-			}
-		}
-
-		/// <summary>
-		/// Adds a handler for processing raw JSON values where there is a regex match.
-		/// </summary>
-		[Obsolete("Use WebSocketWrapper#OnJsonSet instead")]
-		public void AddHandler(Regex regex, RegexJsonSetHandler handler) => regexJsonHandlers.Add((regex, handler));
-
-		/// <summary>
-		/// Adds a handler for processing deserialized values where there is a regex match.
-		/// </summary>
-		[Obsolete("Use WebSocketWrapper#OnJsonSet instead")]
-		public void AddHandler<T>(Regex regex, RegexTypedSetHandler<T> handler) => regexTypedHandlers.Add((regex, handler));
+		wsw.OnJsonSet += OnJsonSet;
 	}
+
+	private void OnJsonSet(string scope, string id, JsonElement jsonElement)
+	{
+		// Invoke handlers that process raw JSON values for exact ID matches
+		if (this.jsonHandlers.TryGetValue(id, out List<JsonSetHandler>? jsonHandlers))
+		{
+			foreach (JsonSetHandler handler in jsonHandlers)
+			{
+				handler(scope, id, jsonElement);
+			}
+		}
+
+		// Invoke handlers that process deserialized values for exact ID matches
+		if (this.typedHandlers.TryGetValue(id, out List<Delegate>? typedHandlers))
+		{
+			foreach (Delegate handler in typedHandlers)
+			{
+				// Get type dynamically from handler
+				Type targetType = handler.GetType().GenericTypeArguments[0];
+				// Deserialize jsonElement with type
+				object? deserializedValue = JsonSerializer.Deserialize(jsonElement, targetType);
+				// Invoke handler
+				handler.DynamicInvoke(scope, id, deserializedValue);
+			}
+		}
+
+		// Invoke handlers that process raw JSON values for regex matched IDs
+		foreach ((Regex regex, RegexJsonSetHandler handler) in regexJsonHandlers)
+		{
+			Match match = regex.Match(id);
+
+			if (match.Success)
+			{
+				handler(scope, match, jsonElement);
+			}
+		}
+
+		// Invoke handlers that process deserialized values for regex matched IDs
+		foreach ((Regex regex, Delegate handler) in regexTypedHandlers)
+		{
+			Match match = regex.Match(id);
+
+			if (match.Success)
+			{
+				// Get type dynamically from handler
+				Type targetType = handler.GetType().GenericTypeArguments[0];
+				// Deserialize jsonElement with type
+				object? deserializedValue = JsonSerializer.Deserialize(jsonElement, targetType);
+				// Invoke handler
+				handler.DynamicInvoke(scope, match, deserializedValue);
+			}
+		}
+	}
+
+	// Publics
+
+	/// <summary>
+	/// Adds a handler for processing raw JSON values where there is an exact ID match.
+	/// </summary>
+	public void AddHandler(string id, JsonSetHandler handler)
+	{
+		if (jsonHandlers.TryGetValue(id, out List<JsonSetHandler>? handlers))
+		{
+			handlers.Add(handler);
+		}
+		else
+		{
+			jsonHandlers[id] = [handler];
+		}
+	}
+
+	/// <summary>
+	/// Adds a handler for processing deserialized values where there is an exact ID match.
+	/// </summary>
+	public void AddHandler<T>(string id, TypedSetHandler<T> handler)
+	{
+		if (typedHandlers.TryGetValue(id, out List<Delegate>? handlers))
+		{
+			handlers.Add(handler);
+		}
+		else
+		{
+			typedHandlers[id] = [handler];
+		}
+	}
+
+	/// <summary>
+	/// Adds a handler for processing raw JSON values where there is a regex match.
+	/// </summary>
+	[Obsolete("Use WebSocketWrapper#OnJsonSet instead")]
+	public void AddHandler(Regex regex, RegexJsonSetHandler handler) => regexJsonHandlers.Add((regex, handler));
+
+	/// <summary>
+	/// Adds a handler for processing deserialized values where there is a regex match.
+	/// </summary>
+	[Obsolete("Use WebSocketWrapper#OnJsonSet instead")]
+	public void AddHandler<T>(Regex regex, RegexTypedSetHandler<T> handler) => regexTypedHandlers.Add((regex, handler));
 }
